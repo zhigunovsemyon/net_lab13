0b66567608cdbf45c22e3d12d460c897f1c16d15
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-12-01 20:11:23 +0300

    Вайтспейсы, переименовал сокет, т.к. больше нет командных сокетов

--- src/main.c
+++ src/main.c
@@ -21,37 +21,39 @@ int main()
 {
 	// Структура с адресом и портом сервера
 	struct sockaddr_in server_addr;
+
 	if (set_destination(&server_addr)) {
 		fputs("Неправильно указан адрес!\n", stderr);
 		return 1;
 	}
+
 	// Входящий сокет
-	fd_t cmd_sock = create_connected_socket(&server_addr);
-	if (-1 == cmd_sock) {
+	fd_t conn_sock = create_connected_socket(&server_addr);
+	if (-1 == conn_sock) {
 		perror("Failed to create command socket");
 		return -1;
 	}
 
-	int login_bad = login(cmd_sock);
+	int login_bad = login(conn_sock);
 	if (login_bad == -1) {
 		perror("login failed");
-		close(cmd_sock);
+		close(conn_sock);
 		return 1;
 	} else if (login_bad == -2) {
 		fprintf(stderr, "Неверный пароль\n");
-		close(cmd_sock);
+		close(conn_sock);
 		return 1;
 	}
 
-	int communication_cycle_bad = communication_cycle(cmd_sock);
+	int communication_cycle_bad = communication_cycle(conn_sock);
 	if (communication_cycle_bad < 0) {
 		perror("cycle failed");
-		close(cmd_sock);
+		close(conn_sock);
 		return 1;
 	}
 
 	// Штатное завершение работы
 	puts("Клиент прервал соединение");
-	close(cmd_sock);
+	close(conn_sock);
 	return 0;
 }
